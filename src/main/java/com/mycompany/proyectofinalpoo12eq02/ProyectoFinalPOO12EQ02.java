49e73eb63ff517488b411d15e3515cf6ce511e49
faann <faann@Pompompurin>
2025-10-20 13:50:29 -0600

    Proyecto POO

---
+++ src/main/java/com/mycompany/proyectofinalpoo12eq02/ProyectoFinalPOO12EQ02.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectofinalpoo12eq02;
+
+/**
+ *
+ * @author faann
+ */
+public class ProyectoFinalPOO12EQ02 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
