64a4044cd108de56cdbf6a56c3211faffbce7a4e
JesusCabanzo06 <cabanzolopezjosedejesus@gmail.com>
2025-11-19 20:47:22 -0600

    Update and rename ClasePrueba.java to AcercaDe.java

---
+++ src/main/java/com/mycompany/proyectofinalpoo12eq02/AcercaDe.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyectofinalpoo12eq02;
+
+
+public class AcercaDe {
+    print("Programacion Orientada a objetos\n");
+    print("Grupo 12\nSemestre 2026-1");
+    print("Equipo ##\n //nombre del equipo //CAMBIAR"); //no se que equipo somos :(
+    print("Integrantes:\nCabanzo Lopez Jose de Jesus.   322138915");
+        print("\Desarrollo: ");
+    print("\nCamacho Ramos Paola Estefany.   #########"); //pongan sus numeros de cuenta, no los pide, pero siento que quedaria bn, o si no, avisenme para quitar el mio xd
+        print("\nDesarrollo: ");
+    print("\nCuevas López José Roberto.   #########");
+        print("\nDesarrollo: ");
+    print("\nLuis Ortiz Déborah Patricia.   #########");
+        print("\nDesarrollo: ");
+    print("\nVargas de la Cruz Alan.   #########");    
+        print("\Desarrollo: ");  
+}
+
+
