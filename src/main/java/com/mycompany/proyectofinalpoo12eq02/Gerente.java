58ef81478478cb732662e0f30d25bf41e9dc5018
IngeCuevas <ingecuevas@comunidad.unam.mx>
2025-12-01 17:30:16 -0600

    Clase Gerente
    Ya tenemos la clase superpadre datospersonas, ya tenemos la clase empleada y cleinte, falta gerente.

---
+++ src/main/java/com/mycompany/proyectofinalpoo12eq02/Gerente.java
@@ -0,0 +1,19 @@
+package com.mycompany.proyectofinalpoo12eq02;
+// Esta clase representa a un GERENTE de CINE-TICs.
+
+// * Según los requerimientos del PDF (pág. 5), el gerente tiene: 
+ //   -permisos avanzados del sistema:
+ //   - Administrar usuarios
+ //   - Consultar y actualizar inventarios
+ //  - Ver estadísticas del negocio
+ //  - Generar reportes de ventas
+ //  - Iniciar la simulación de una función 
+
+// *Además, se exige para los empleados y gerentes:
+ //- RFC
+ //- Número de trabajador
+ //- Sucursal donde labora
+ // Por eso esta clase agrega esos campos extras.
+
+public class Gerente extends datosPersonas {
+ 
